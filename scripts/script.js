84cc78b3a1519e1e9536651da4b742cbccde090f
cjmDevelop <jr87.dev@gmail.com>
2025-07-01 16:17:31 -0400

    Bricks in quiz pyramid now populates with real numbered quiz data.

--- scripts/script.js
+++ scripts/script.js
@@ -6,5 +6,33 @@ import { fifthQuiz } from './fifth-quiz.js'
 import { sixthQuiz } from './sixth-quiz.js'
 import { seventhQuiz } from './seventh-quiz.js'
 
-console.log(firstQuiz);
+const quizzes = [firstQuiz, secondQuiz, thirdQuiz, fourthQuiz, fifthQuiz, sixthQuiz, seventhQuiz];
+
+const bricks = document.querySelectorAll('.brick');
+      bricks.forEach((brick, index) => {
+        if(quizzes[index]) {
+            brick.textContent = index + 1;
+        } else {
+            brick.textContent = '';
+        }
+      });
+
+/** Test Example
+const box = document.getElementById('test-click');
+const infoBox = document.getElementById('info-box');
+let infoShown = false;
+
+box.addEventListener('click', () => {
+    if(!infoShown){
+        infoBox.style.display = 'block';
+        infoBox.textContent = 'This is only a test.'
+        infoShown = true;
+    } else {
+        infoBox.style.display = 'none';
+        infoBox.textContent = '';
+        infoShown = false;
+    }
+    
+});
+*/
 
