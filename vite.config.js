daf56bdd05b440e9789e355ded378e0fbf5401b0
Claude Joseph Manbodh Jr. 'Joe <63468241+cjmDevelop@users.noreply.github.com>
2025-12-19 16:10:06 -0500

    manually adding html files to vit config in order for netlify to find html files other then index.html

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,30 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     server: {
         port: 3000,
         open: true
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                about: resolve(__dirname, 'about.html'),
+                login: resolve(__dirname, 'login.html'),
+                signup: resolve(__dirname, 'signup.html'),
+                profile: resolve(__dirname, 'profile.html'),
+                'verify-email': resolve(__dirname, 'verify-email.html'),
+                'forgot-password': resolve(__dirname, 'forgot-password.html'),
+                'reset-password': resolve(__dirname, 'reset-password.html'),
+                'quiz-menu': resolve(__dirname, 'quiz-menu.html'),
+                quiz: resolve(__dirname, 'quiz.html'),
+                'sub-menu': resolve(__dirname, 'sub-menu.html'),
+                'sub-menu-2': resolve(__dirname, 'sub-menu-2.html'),
+                'sub-menu-3': resolve(__dirname, 'sub-menu-3.html'),
+                'sub-menu-4': resolve(__dirname, 'sub-menu-4.html'),
+                'sub-menu-5': resolve(__dirname, 'sub-menu-5.html'),
+            }
+        }
     }
 })
